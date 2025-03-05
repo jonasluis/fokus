fd530b90a23ff5bfb2af49b053f934ee876000c4
jonasluis <jonasluis66@gmail.com>
2025-03-05 10:06:57 -0300

    feat: alterando imagens

--- script.js
+++ script.js
@@ -2,15 +2,22 @@ const html =  document.querySelector('html');
 const focoBotao = document.querySelector('.app__card-button--foco');
 const descansoCurtoBotao = document.querySelector('.app__card-button--curto');
 const descansoLongoBotao = document.querySelector('.app__card-button--longo');
+const banner = document.querySelector('.app__image');
 
 focoBotao.addEventListener('click', () => {
     html.setAttribute('data-contexto', 'foco');
+    banner.setAttribute('src', '/imagens/foco.png')
 })
 
 descansoCurtoBotao.addEventListener('click', () => {
     html.setAttribute('data-contexto', 'descanso-curto');
+    banner.setAttribute('src', '/imagens/descanso-curto.png')
 })
 
 descansoLongoBotao.addEventListener('click', () => {
     html.setAttribute('data-contexto', 'descanso-longo');
+    banner.setAttribute('src', '/imagens/descanso-longo.png')
 })
+
+ 
+
