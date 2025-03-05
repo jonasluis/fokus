f4feb59bd3f4a91dee5dfdcf409484dd84a4f076
jonasluis <jonasluis66@gmail.com>
2025-03-05 10:23:44 -0300

    feat: alterando texto da página

--- script.js
+++ script.js
@@ -3,22 +3,39 @@ const focoBotao = document.querySelector('.app__card-button--foco');
 const descansoCurtoBotao = document.querySelector('.app__card-button--curto');
 const descansoLongoBotao = document.querySelector('.app__card-button--longo');
 const banner = document.querySelector('.app__image');
+const titulo = document.querySelector('.app__title');
 
 focoBotao.addEventListener('click', () => {
-    alterarContexto('foco')
+    alterarContexto('foco');
 })
 
 descansoCurtoBotao.addEventListener('click', () => {
-    alterarContexto('descanso-curto')
+    alterarContexto('descanso-curto');
 })
 
 descansoLongoBotao.addEventListener('click', () => {
-    alterarContexto('descanso-longo')
+    alterarContexto('descanso-longo');
 })
 
 function alterarContexto(contexto) {
     html.setAttribute('data-contexto', contexto);
-    banner.setAttribute('src', `/imagens/${contexto}.png`)
+    banner.setAttribute('src', `/imagens/${contexto}.png`);
+
+    switch (contexto) {
+        case "foco":
+                titulo.innerHTML = `Otimize sua produtividade,<br>
+                <strong class="app__title-strong">mergulhe no que importa.</strong>`;
+            break;
+        case "descanso-curto":
+            titulo.innerHTML = `Que tal da uma respirada?<br>
+            <strong class="app__title-strong">Faça uma pausa curta!</strong>`;
+            break;
+        case "descanso-longo":
+            titulo.innerHTML = `Hora de voltar à superfície.<br>
+            <strong class="app__title-strong">Faça uma pausa longa.</strong>`;
+        default:
+            break;
+    }
 }
- 
+
 
