c989021e233cf7d1a3947266593f79f22ac05305
jonasluis <jonasluis66@gmail.com>
2025-03-05 10:41:42 -0300

    feat: inserindo objeto de áudio

--- script.js
+++ script.js
@@ -5,25 +5,36 @@ const descansoLongoBotao = document.querySelector('.app__card-button--longo');
 const banner = document.querySelector('.app__image');
 const titulo = document.querySelector('.app__title');
 const botoes = document.querySelectorAll('.app__card-button');
+const musicaFocoInput = document.querySelector('#alternar-musica');
+const musica = new Audio('/sons/luna-rise-part-one.mp3');
+musica.loop = true
+
+musicaFocoInput.addEventListener('change', () => {
+    if(musica.paused) {
+        musica.play();
+    }else {
+        musica.pause();
+    }
+})
 
 focoBotao.addEventListener('click', () => {
     alterarContexto('foco');
     focoBotao.classList.add('active');
-})
+});
 
 descansoCurtoBotao.addEventListener('click', () => {
     alterarContexto('descanso-curto');
     descansoCurtoBotao.classList.add('active');
-})
+});
 
 descansoLongoBotao.addEventListener('click', () => {
     alterarContexto('descanso-longo');
     descansoLongoBotao.classList.add('active');
-})
+});
 
 function alterarContexto(contexto) {
     botoes.forEach(function (contexto){
-        contexto.classList.remove('active')
+        contexto.classList.remove('active');
     });
     html.setAttribute('data-contexto', contexto);
     banner.setAttribute('src', `/imagens/${contexto}.png`);
